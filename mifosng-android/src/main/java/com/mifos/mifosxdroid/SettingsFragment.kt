490063e9466a46ed39bba05e01ccddd5e5278a08
Usama.Yasin <uyasin@pie-int.com>
2021-05-21 23:12:43 +0500

    Settings Screen UI Updated.

--- mifosng-android/src/main/java/com/mifos/mifosxdroid/SettingsFragment.kt
+++ mifosng-android/src/main/java/com/mifos/mifosxdroid/SettingsFragment.kt
@@ -1,45 +1,69 @@
 package com.mifos.mifosxdroid
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
-import android.preference.ListPreference
-import android.preference.Preference.OnPreferenceChangeListener
 import android.preference.PreferenceFragment
 import android.preference.SwitchPreference
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.mifos.mifosxdroid.dialogfragments.syncsurveysdialog.SyncSurveysDialogFragment
 import com.mifos.utils.FragmentConstants
 import com.mifos.utils.ThemeHelper
-
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 /**
  * Created by mayankjindal on 22/07/17.
  */
 class SettingsFragment : PreferenceFragment() {
-    var mEnableSyncSurvey: SwitchPreference? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
 
+    lateinit var rootView: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        addPreferencesFromResource(R.xml.preferences)
-        mEnableSyncSurvey = findPreference(resources.getString(R.string.sync_survey)) as SwitchPreference
-        mEnableSyncSurvey!!.onPreferenceChangeListener =
-                OnPreferenceChangeListener { preference, newValue ->
-            if (newValue as Boolean) {
-                val syncSurveysDialogFragment = SyncSurveysDialogFragment.newInstance()
-                val fragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.addToBackStack(FragmentConstants.FRAG_SURVEYS_SYNC)
-                syncSurveysDialogFragment.isCancelable = false
-                syncSurveysDialogFragment.show(fragmentTransaction,
-                        resources.getString(R.string.sync_clients))
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        rootView = inflater.inflate(R.layout.fragment_settings, container, false)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        sc_sync_survey.setOnClickListener {
+            showSurveyDialog()
+        }
+        sc_dark_mode.setOnClickListener {
+            showDarkModeDialog()
+        }
+    }
+
+    fun showDarkModeDialog() {
+        val alertDialog: AlertDialog.Builder = AlertDialog.Builder(rootView.context)
+        alertDialog.setTitle("AlertDialog")
+        val items = arrayOf("Light", "Dark", "Default")
+        val checkedItem = 0
+        alertDialog.setSingleChoiceItems(items, checkedItem, DialogInterface.OnClickListener { dialog, which ->
+            when (which) {
+                0 -> ThemeHelper.applyTheme(ThemeHelper.LIGHT_MODE)
+                1 -> ThemeHelper.applyTheme(ThemeHelper.DARK_MODE)
+                2 -> ThemeHelper.applyTheme(ThemeHelper.DEFAULT_MODE)
             }
-            true
-        }
-        val themePreference = findPreference("dark_mode") as ListPreference
-        themePreference.onPreferenceChangeListener = OnPreferenceChangeListener { preference, newValue ->
-            val themeOption = newValue as String
-            ThemeHelper.applyTheme(themeOption)
             startActivity(Intent(activity, activity.javaClass))
-            true
-        }
+        })
+        val alert: AlertDialog = alertDialog.create()
+        alert.show()
+    }
+
+    fun showSurveyDialog() {
+        val syncSurveysDialogFragment = SyncSurveysDialogFragment.newInstance()
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.addToBackStack(FragmentConstants.FRAG_SURVEYS_SYNC)
+        syncSurveysDialogFragment.isCancelable = false
+        syncSurveysDialogFragment.show(fragmentTransaction,
+                resources.getString(R.string.sync_clients))
     }
 
     companion object {
